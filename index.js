0e60ca51a94279a6d6d9055cdf6ae2d8ffa57882
filipakakacper <kacper.filipiak890@gmail.com>
2023-11-15 13:44:13 +0100

    gotowe

---
+++ index.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "fd06aadd5bf10c",
+      pass: "14d57d3c43e7f3"
+    }
+  });
+
+const mailOptions = {
+  from: 'your_email@gmail.com',
+  to: 'recipient_email@example.com',
+  subject: 'Temat wiadomości',
+  text: 'Treść wiadomości'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('E-mail wysłany: ' + info.response);
+  }
+});
